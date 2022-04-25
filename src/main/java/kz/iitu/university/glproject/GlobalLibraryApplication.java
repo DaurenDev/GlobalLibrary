db5ec27691a24927c38d0814edffca0c27c0e59f
Dauren <dauren141001@gmail.com>
2022-04-25 23:28:15 +0600

    Initial commit

---
+++ src/main/java/kz/iitu/university/glproject/GlobalLibraryApplication.java
@@ -0,0 +1,13 @@
+package kz.iitu.university.glproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlobalLibraryApplication.class, args);
+    }
+
+}
